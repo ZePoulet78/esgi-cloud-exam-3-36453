1f40a0f9a219456444c73889b7b7e1c95d5b601b
ZePoulet78 <vdonnesh@gmail.com>
2024-04-02 15:40:26 +0200

    Edit config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: '3CZ0853NE9GU5VBA5EHF', // TODO
+    secretAccessKey: 'ZuZ9QaggzwdUFlPlDI4mRq2B6ue0zur6dQiqlC7u', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-36453', // TODO
   },
   suffix: {
     small: '_small.jpg',
